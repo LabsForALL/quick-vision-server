f41bf8db73258a24b314803cc078a05263290c79
LabsForALL <onefor91@gmail.com>
2017-07-21 01:22:41 +0300

    added opencv and tested with mobile phone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import socket
+import numpy as np
 import zlib
+import cv2
+
 
 class Packet:
     img_data = 0
@@ -13,27 +16,22 @@ class Packet:
         self.packet_sum = int.from_bytes(data[12:16], byteorder='big')
         self.img_data = data[16:497]
 
-    def show_info(self):
-        print("Time stamp: " + str(self.time_stamp))
-        print("Packet number: " + str(self.packet_number))
-        print("Packet count: " + str(self.packet_sum))
-        print("Data: " + str(self.img_data))
-
 
 class PacketsManager:
-    packets = {}
+    packets_dic = {}
     frames = {}
 
     def add_packet(self, data):
-        packet = Packet(data)
-        ts = packet.time_stamp
+        new_packet = Packet(data)
+        new_ts = new_packet.time_stamp
 
-        if ts in packets:
-            packets[ts].append(packet)
-            if len(packets[ts]) == packet.packet_sum:
-                self.create_frame(packets[ts])
+        if new_ts in packets_dic:
+            packets_dic[new_ts].append(new_packet)
+            if len(packets_dic[new_ts]) == new_packet.packet_sum:
+                self.create_frame(packets_dic[new_ts])
+                del packets_dic[new_ts]
         else:
-            packets[ts] = [packet]
+            packets_dic[new_ts] = [new_packet]
 
     def create_frame(self, packets):
         pks = sorted(packets, key=lambda x: x.packet_number)
@@ -43,17 +41,22 @@ class PacketsManager:
             img_data.extend(pks[i].img_data)
             i += 1
         jpeg_data = zlib.decompress(img_data)
-        with open('picture_out.jpg', 'wb') as f:
-            f.write(jpeg_data)
+
+        nparr = np.fromstring(jpeg_data, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+        cv2.imshow(window_title, img_np)
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 packets_manager = PacketsManager()
-packets = {}
+packets_dic = {}
 
 sock.bind(("", 10000))
 print("server started!")
 
+window_title = 'preview'
+cv2.startWindowThread()
+cv2.namedWindow(window_title)
 
 while True:
     data, address = sock.recvfrom(496) # buffer size is 1024 bytes
