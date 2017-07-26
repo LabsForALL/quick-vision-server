887e8fc5586eb16b4849bc71b148fcf89d9a4ffb
LabsForALL <onefor91@gmail.com>
2017-07-26 04:50:27 +0300

    added neural network processing

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import cv2
 import json
 import base64
 import time
-
+from multiprocessing import Process, Queue
+import threading
 
 class Packet:
     img_data = 0
@@ -21,79 +22,104 @@ class Packet:
         self.img_data = data[16:497]
 
 
-packets_dic = {}
-model_dir = 'exported_model/'
+def process_frames(q: Queue):
 
-# restoring the exported model
-sess = tf.Session()
-saver = tf.train.import_meta_graph(model_dir + "export.meta")
-saver.restore(sess, model_dir + "export")
-input_vars = json.loads(tf.get_collection("inputs")[0].decode('utf-8'))
-output_vars = json.loads(tf.get_collection("outputs")[0].decode('utf-8'))
-input = tf.get_default_graph().get_tensor_by_name(input_vars["input"])
-output = tf.get_default_graph().get_tensor_by_name(output_vars["output"])
-print("model ready!")
+    stopped = False
+    last_frame = None;
 
-# starting UDP server
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", 10000))
-print("server started!")
+    # restoring the exported model
+    model_dir = 'exported_model/'
+    sess = tf.Session()
+    saver = tf.train.import_meta_graph(model_dir + "export.meta")
+    saver.restore(sess, model_dir + "export")
+    input_vars = json.loads(tf.get_collection("inputs")[0].decode('utf-8'))
+    output_vars = json.loads(tf.get_collection("outputs")[0].decode('utf-8'))
+    input = tf.get_default_graph().get_tensor_by_name(input_vars["input"])
+    output = tf.get_default_graph().get_tensor_by_name(output_vars["output"])
+    print("model ready!")
 
-window_title = 'preview'
-cv2.startWindowThread()
-cv2.namedWindow(window_title)
+    window_title = 'preview'
+    cv2.startWindowThread()
+    cv2.namedWindow(window_title)
 
-while True:
-    # waiting for packets
-    data, address = sock.recvfrom(496) # buffer size is 1024 bytes
-    new_packet = Packet(data)
-    new_ts = new_packet.time_stamp
+    while not stopped:
 
-    if new_ts in packets_dic:
-        packets_dic[new_ts].append(new_packet)
+        frame_data = q.get()
 
-        if len(packets_dic[new_ts]) == new_packet.packet_sum:
+        print(frame_data)
 
-            t1 = time.time()
+        # processing with the neural network
+        input_instance = dict(input=base64.urlsafe_b64encode(frame_data).decode("ascii"), key="0")
+        input_instance = json.loads(json.dumps(input_instance))
+        input_value = np.array(input_instance["input"])
 
-            # building the frame from packets
-            s_packets = sorted(packets_dic[new_ts], key=lambda x: x.packet_number)
-            img_data = bytearray()
-            i = 0
-            while i < len(s_packets):
-                img_data.extend(s_packets[i].img_data)
-                i += 1
+        output_value = sess.run(output, feed_dict={input: np.expand_dims(input_value, axis=0)})[0]
+        output_instance = dict(output=output_value.decode("ascii"), key="0")
+        b64data = output_instance["output"]
+        b64data += "=" * (-len(b64data) % 4)
+        output_data = base64.urlsafe_b64decode(b64data.encode("ascii"))
+        nparr = np.fromstring(output_data, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+        cv2.imshow(window_title, img_np)
 
-            i = len(img_data) - 1
-            while True:
-                if img_data[i] != 0:
-                    break
-                i -= 1
-            del img_data[i+1:]
 
-            jpeg_data = zlib.decompress(img_data)
+if __name__ == '__main__':
 
-            # processing with the neural network
-            input_instance = dict(input=base64.urlsafe_b64encode(jpeg_data).decode("ascii"), key="0")
-            input_instance = json.loads(json.dumps(input_instance))
-            input_value = np.array(input_instance["input"])
+    # starting UDP server
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", 10000))
+    print("server started!")
 
-            output_value = sess.run(output, feed_dict={input: np.expand_dims(input_value, axis=0)})[0]
+    packets_dic = {}
+    queue = Queue()
+    p = Process(target=process_frames, args=(queue,))
+    p.start()
 
-            output_instance = dict(output=output_value.decode("ascii"), key="0")
-            b64data = output_instance["output"]
-            b64data += "=" * (-len(b64data) % 4)
-            output_data = base64.urlsafe_b64decode(b64data.encode("ascii"))
+    while True:
 
-            t2 = time.time()
-            print('process time: ' + str(t2 - t1))
+        # waiting for packets
+        ts1 = time.time()
+        data, address = sock.recvfrom(496)  # buffer size is 496 bytes
+        new_packet = Packet(data)
+        new_ts = new_packet.time_stamp
 
-            nparr = np.fromstring(output_data, np.uint8)
-            img_np = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
-            cv2.imshow(window_title, img_np)
+        if new_ts in packets_dic:
+            packets_dic[new_ts].append(new_packet)
 
-            del packets_dic[new_ts]
-    else:
-        packets_dic[new_ts] = [new_packet]
+            # checking if frame is ready
+            if len(packets_dic[new_ts]) == new_packet.packet_sum:
 
-    print(address)
+                # building the frame from packets
+                s_packets = sorted(packets_dic[new_ts], key=lambda x: x.packet_number)
+                img_data = bytearray()
+                i = 0
+                while i < len(s_packets):
+                    img_data.extend(s_packets[i].img_data)
+                    i += 1
+
+                # i = len(img_data) - 1
+                # while True:
+                #     if img_data[i] != 0:
+                #         break
+                #     i -= 1
+                # del img_data[i+1:]
+
+                jpeg_data = zlib.decompress(img_data)
+
+                queue.put(jpeg_data)
+
+                # clean up
+                del packets_dic[new_ts]
+
+                for packet in packets_dic:
+                    if packets_dic[packet].time_stamp < new_ts:
+                        del packet
+        else:
+            packets_dic[new_ts] = [new_packet]
+
+        print('Received packet in ' + str(time.time() - ts1))
+
+    # Wait for the worker to finish
+    # queue.close()
+    # queue.join_thread()
+    # p.join()
